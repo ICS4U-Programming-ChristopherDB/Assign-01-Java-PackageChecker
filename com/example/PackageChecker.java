48b765c083440fea4d5970d2d979b9be9850dcd6
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-03-20 21:29:53 +0000

    lint test

--- com/example/PackageChecker.java
+++ com/example/PackageChecker.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 // PackageChecker class
 public final class PackageChecker {
 
+  // Used in program loop.
   private static boolean playAgain = true;
 
   private static String[] massUnits = {"lbs", "kg", "g"};
@@ -48,45 +49,64 @@ public final class PackageChecker {
     }
   }
 
-    /**
-   * Master method that gets all user input.
-   * Returns a boolean value to indicate successful operation.
+  /**
+   * Master method that gets all user input. Returns a boolean value to indicate successful
+   * operation.
+   *
    * @return executedSuccessfully.
    */
-    private static boolean GetUserValues() {
+  private static boolean GetUserValues() {
     // Dumb variable to please linter. Linter does not like returning raw data.
     boolean executedSuccessfully;
 
     Scanner valuesScanner = new Scanner(System.in);
+
+    // Calls method that presents all units in a list.
     PresentOptionsMenu("MASS UNITS", massUnits);
     System.out.print("Enter the mass unit you would like to use:\n> ");
+
+    /*Gets the user's mass unit and passes it to a method that
+     * checks if the user's input was valid.
+     */
     userMassUnit = GetChoice(valuesScanner.nextLine(), massUnits);
 
+    /*If GetChoice did not execute successfully, tell the user they
+     * made a mistake and prompt the user to restart.
+     */
     if (userMassUnit.equals("Choice does not exist")) {
-        System.out.println("\nYou must select a valid mass unit!");
-        executedSuccessfully = false;
-        return executedSuccessfully;
+      System.out.println("\nYou must select a valid mass unit!");
+      executedSuccessfully = false;
+      return executedSuccessfully;
     }
 
+    // Same process but for length units...
     PresentOptionsMenu("LENGTH UNITS", lengthUnits);
     System.out.print("Enter the length unit you would like to use:\n> ");
     userLengthUnit = GetChoice(valuesScanner.nextLine(), lengthUnits);
 
     if (userLengthUnit.equals("Choice does not exist")) {
-        System.out.println("\nYou must select a valid length unit!");
-        executedSuccessfully = false;
-        return executedSuccessfully;
+      System.out.println("\nYou must select a valid length unit!");
+      executedSuccessfully = false;
+      return executedSuccessfully;
     }
 
     if (!GetMeasurements()) {
-        System.out.println("\nYou must enter numbers for measurements!");
-        executedSuccessfully = false;
-        return executedSuccessfully;
+      System.out.println("\nYou must enter numbers for measurements!");
+      executedSuccessfully = false;
+      return executedSuccessfully;
     }
+
+    // If no errors were found, exit successfully.
     executedSuccessfully = true;
     return executedSuccessfully;
   }
 
+  /**
+   * Simple method that generates a titled list of options from an array of strings.
+   *
+   * @param title passed.
+   * @param options passed.
+   */
   private static void PresentOptionsMenu(String title, String[] options) {
     System.out.println("\n===== " + title + " =====");
     for (int i = 0; i < options.length; i++) {
@@ -94,28 +114,48 @@ public final class PackageChecker {
     }
   }
 
+  /**
+   * Method that gets the user's choice from a list. It accepts user input in the form of an integer
+   * or string and performs comparisons accordingly.
+   *
+   * @param userChoice passed.
+   * @param options passed.
+   * @return returnMessage.
+   */
   private static String GetChoice(String userChoice, String[] options) {
+    // Dumb variable to please linter.
+    String returnMessage;
     int userChoiceNum;
     for (String option : options) {
       if (userChoice.equals(option)) {
-        return option;
+        returnMessage = option;
+        return returnMessage;
       }
     }
 
     try {
       userChoiceNum = Integer.parseInt(userChoice) - 1;
     } catch (Exception e) {
-      return "Choice does not exist";
+      returnMessage = "Choice does not exist";
+      return returnMessage;
     }
 
     if (userChoiceNum < options.length && userChoiceNum > options.length) {
-      return "Choice does not exist";
+      returnMessage = "Choice out of bounds exist";
+      return returnMessage;
     }
 
-    return options[userChoiceNum];
+    returnMessage = options[userChoiceNum];
+    return returnMessage;
   }
 
+  /**
+   * Method that gets the user's measurements.
+   *
+   * @return returnMessage.
+   */
   private static boolean GetMeasurements() {
+    boolean executedSuccessfully;
     Scanner dimensionScanner = new Scanner(System.in);
     String[] measurements = {"length", "width", "height"};
     for (int i = 0; i < measurements.length; i++) {
@@ -125,18 +165,27 @@ public final class PackageChecker {
         double userDim = Double.parseDouble(dimensionScanner.nextLine());
         packageDimensions[i] = userDim;
       } catch (Exception e) {
-        return false;
+        executedSuccessfully = false;
+        return executedSuccessfully;
       }
     }
     System.out.print("\nEnter the package mass in " + userMassUnit + "\n> ");
     try {
       packageMass = dimensionScanner.nextDouble();
     } catch (Exception e) {
-      return false;
+      executedSuccessfully = false;
+      return executedSuccessfully;
     }
-    return true;
+    executedSuccessfully = true;
+    return executedSuccessfully;
   }
 
+  /**
+   * Method that checks if the package can be loaded. It converts the units provided by the user
+   * into kilograms and centimeters and checks if the package can be loaded.
+   *
+   * @return returnMessage.
+   */
   private static void PackageCanBeLoaded() {
     double volume = 1;
     for (Double dimension : packageDimensions) {
@@ -154,7 +203,7 @@ public final class PackageChecker {
         break;
     }
 
-    if (volume > 10000) {
+    if (volume > 10000d) {
       System.out.println("Cannot send package! It is too large!");
     }
 
@@ -176,6 +225,7 @@ public final class PackageChecker {
     }
   }
 
+  /** Method that resets all user values before restarting. */
   private static void ResetValues() {
     packageDimensions = new Double[3];
     packageMass = 0d;
